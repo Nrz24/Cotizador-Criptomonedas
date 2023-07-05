d7647a577ca18d5f33ebe4f21ae9bd28743b7909
Nicolas Ramirez <nico13544@gmail.com>
2023-07-05 20:02:29 +0200

    Primer commit para deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+Aplicacion para cotizar las criptomonedas mas importantes, utilizando APIs
---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar Americano'},
+    {id: 'MXN', nombre: 'Peso Mexicano'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GPR', nombre: 'Libra Esterlina'},
+]
+
+export {
+    monedas
+}
+
+export default monedas
